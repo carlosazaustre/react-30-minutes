2df08e2c25bdf33c9d4b62db1cf3a385b5221bb2
Carlos Azaustre <cazaustre@gmail.com>
2021-06-28 14:20:35 +0200

    Add Todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { TodoList } from "./components/TodoList";
 
 export function App() {
+  const todoTaskRef = useRef();
   const [todos, setTodos] = useState([
     { id: 1, task: "Tarea ", completed: false },
   ]);
 
+  const handleTodoAdd = (event) => {
+    const task = todoTaskRef.current.value;
+    if (task === "") return;
+
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: uuidv4(), task, completed: false }];
+    });
+
+    todoTaskRef.current.value = null;
+  };
+
   return (
     <Fragment>
       <TodoList todos={todos} />
-      <input type="text" placeholder="Nueva tarea" />
-      <button>Añadir</button>
+      <input ref={todoTaskRef} type="text" placeholder="Nueva tarea" />
+      <button onClick={handleTodoAdd}>Añadir</button>
       <button>Eliminar</button>
     </Fragment>
   );
